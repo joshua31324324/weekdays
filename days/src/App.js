fd1d7b7b281cdd201d662af597640d838fb691ce
joshua <jo336@njit.edu>
2025-05-11 17:31:05 -0400

    changed some things that didn't seem right to me

--- days/src/App.js
+++ days/src/App.js
@@ -5,17 +5,29 @@ function App() {
   // Call the dayChecker function to get the current day
   const {day, link, image, title} = dayChecker();
   const today = {day, link, image, title};
-  const todayNumber = new Date().getDay();
-
-
+  const todayNumber = new Date().getDay() + 1;
+  let suffix;
+  switch (todayNumber) {
+    case 1:
+      suffix = "st";
+      break;
+    case 2:
+      suffix = "nd";
+      break;
+    case 3:
+      suffix = "rd";
+      break;
+    default:
+      suffix = "th";
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-left">
         <img src={today.image} alt="Weekday" title={today.title} />
       </header>
-      <div className="App-content">
+      <div className="App-right">
       <h1>Today is {today.day}!</h1>
-      <p>{today.day} is the {todayNumber}th day of the week. Would you like to know more? <a href={today.link}>Click here.</a></p>
+      <p>{today.day} is the {todayNumber}{suffix} day of the week. Would you like to know more? <a href={today.link}>Click here.</a></p>
       </div>
       <footer className="App-footer">
         <p>Created by Joshua Oppong | © 2025 | All rights reserved.</p>
